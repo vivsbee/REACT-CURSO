aa914da8119f829b3d79cf787826af59b792654c
Lina Viviana Garnica <aileen25italia@gmail.com>
2025-05-14 03:07:14 -0500

    Agregué los archivos proyectoCra, proyectoVite y fundamentos creados en la 2da clase

---
+++ fundamentos/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+// App.jsx -> App.js EL COMPONENTE principal
+// Componente en React -> Una función de Javascript que retornan contenido HTML 
+
+
+// SINTAXIS JSX
+function App() {
+
+  return(
+    // comentario de js -> parte de Lógica
+    // Fragment -> cajita -> ponga el contenido de su componente y no genere ningún elemento en el DOM
+    
+    <>
+      {/*comentario en la vista -> HTML */}
+      <h1>Hola Mundo</h1>
+      <h1>Holi, estoy en la clase de fundamentos xd</h1>
+    </>
+  )
+}
+
+export default App
